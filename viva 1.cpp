6802bd883aa4d96968e0e7fb5262294256ce3421
Vasu <vasugarg12345@gmail.com>
2020-10-28 13:36:45 +0530

    C++ code

---
+++ viva 1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class person
+{
+	public:
+		person()
+		{
+			cout << "Name of the person : " << endl;
+		}
+};
+class student
+{
+	public:
+		student()
+		{
+			cout << "Name of the student : " << endl;
+		}
+};
+class faculty
+{
+	public:
+		faculty()
+		{
+			cout << "Name of the faculty : " << endl;
+		}
+};
+int main()
+{
+	student c;
+	return 0;
+}
